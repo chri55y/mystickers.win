57cae0597ff892838bce4dd4977e2099ab3f38a5
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-01-03 15:17:39 -0600

    create functional, stateless child component ProductsList (child to parent Store)

--- app/javascript/packs/Store/index.js
+++ app/javascript/packs/Store/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
+import ProductsList from './ProductsList'
 
 class Store extends React.Component {
     render () {
         return (
             <div>
-                {this.props.products.map(prod => {
-                    return (
-                        <h2 key={prod.id}
-                            // className="ch-product"
-                        >
-                            {prod.name}
-                        </h2>
-                    )
-                })}
+                <ProductsList products={this.props.products}/>
+                {/*{this.props.products.map(prod => {*/}
+                    {/*return (*/}
+                        {/*<h2 key={prod.id}*/}
+                            {/*// className="ch-product"*/}
+                        {/*>*/}
+                            {/*{prod.name}*/}
+                        {/*</h2>*/}
+                    {/*)*/}
+                {/*})}*/}
             </div>
         )
     }
