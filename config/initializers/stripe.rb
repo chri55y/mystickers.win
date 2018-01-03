850562d6d0be60006a884b7f8394f2c9b33469bb
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-01-02 18:55:03 -0600

    stripe setup continued | modify keys, and update bash_profile | page loads, but test transaction fails 'did not set valid publishable key'

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,7 @@
 Rails.configuration.stripe = {
-    :publishable_key => ENV['PUBLISHABLE_KEY'],
-    :secret_key      => ENV['SECRET_KEY']
-}
+    :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+    :secret_key      => ENV['STRIPE_SECRET_KEY']
+}                           # add STRIPE_ prefix for differentiation
+                            # also added these to ~/.bash_profile
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
