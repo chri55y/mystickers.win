cba1f15b65ff1ad13d5ac1c9a8021dadf0c030e1
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-01-03 12:35:39 -0600

    add App.js code. selected to change language to JSX (in RubyMine), error warnings went away, but file extension stayed the same

---
+++ app/javascript/packs/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Store from './Store'
+// knows to reference index file in this directory
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Store />
+            </div>
+        )
+    }
+}
+
+export default App
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(<App />, document.getElementById('store'))
+})
