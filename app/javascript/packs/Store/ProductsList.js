28ac550fd8247c2956104ea8de7ac7cd87d726b5
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-01-03 15:30:05 -0600

    syntax check : functional component declaration wrapped in {} must also include return. so const Component = (props) => <div></div> is the same as => { return ( <div></div> ) }

--- app/javascript/packs/Store/ProductsList.js
+++ app/javascript/packs/Store/ProductsList.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 // stateless functional component
-const ProductsList = ({products}) =>
-
+const ProductsList = ({products}) => {
+    return (
     <div>
         {products.map(prod => {
             return (<h2 key={prod.id}>{prod.name}</h2>)
         })}
     </div>
-
+    )}
 
 
 export default ProductsList
