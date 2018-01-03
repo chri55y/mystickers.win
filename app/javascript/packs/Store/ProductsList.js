41b9e47a0d1801051587032308e680c64ab3af4f
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-01-03 15:21:55 -0600

    clean up code, and test to make sure that props.products can be used instead of products in ProductsList

--- app/javascript/packs/Store/ProductsList.js
+++ app/javascript/packs/Store/ProductsList.js
@@ -1,15 +1,11 @@
 import React from 'react'
 
 // stateless functional component
-const ProductsList = ({products}) =>
+const ProductsList = (props) =>
 
     <div>
-        {products.map(prod => {
-            return (
-                <h2 key={prod.id}>
-                    {prod.name}
-                </h2>
-            )
+        {props.products.map(prod => {
+            return (<h2 key={prod.id}>{prod.name}</h2>)
         })}
     </div>
 
--- app/javascript/packs/Store/index.js
+++ app/javascript/packs/Store/index.js
@@ -6,15 +6,6 @@ class Store extends React.Component {
         return (
             <div>
                 <ProductsList products={this.props.products}/>
-                {/*{this.props.products.map(prod => {*/}
-                    {/*return (*/}
-                        {/*<h2 key={prod.id}*/}
-                            {/*// className="ch-product"*/}
-                        {/*>*/}
-                            {/*{prod.name}*/}
-                        {/*</h2>*/}
-                    {/*)*/}
-                {/*})}*/}
             </div>
         )
     }
