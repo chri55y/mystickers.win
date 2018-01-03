ee5da86618019a2cd37cf62e6cdad6d965b27f51
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-01-03 15:30:49 -0600

    change back less verbose syntax

--- app/javascript/packs/Store/ProductsList.js
+++ app/javascript/packs/Store/ProductsList.js
@@ -1,14 +1,12 @@
 import React from 'react'
 
 // stateless functional component
-const ProductsList = ({products}) => {
-    return (
+const ProductsList = ({products}) =>
     <div>
         {products.map(prod => {
             return (<h2 key={prod.id}>{prod.name}</h2>)
         })}
     </div>
-    )}
 
 
 export default ProductsList
