058fa08fa8b1b307c4800e292a56ffee32b888be
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-01-02 19:02:48 -0600

    stripe setup continued | success: add notes on fixing 'did not set valid publishable key' issue to stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,5 +3,9 @@ Rails.configuration.stripe = {
     :secret_key      => ENV['STRIPE_SECRET_KEY']
 }                           # add STRIPE_ prefix for differentiation
                             # also added these to ~/.bash_profile
+                            #
+                            # error fix: for invalid key
+                            # run command source ~/.bash_profile
+                            # then re-start rails server (same terminal?)
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
