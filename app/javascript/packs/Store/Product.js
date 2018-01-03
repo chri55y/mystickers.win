6def0c1a806bb6dce671806624d649f422731ebe
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-01-03 15:57:08 -0600

    create Product component for rendering individual products. currently just displays the product name

---
+++ app/javascript/packs/Store/Product.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Product = ({product}) =>
+    <div>
+        <h2>{product.name}</h2>
+        {/*<h3>please</h3>*/}
+    </div>
+
+export default Product
--- app/javascript/packs/Store/ProductsList.js
+++ app/javascript/packs/Store/ProductsList.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Product from './Product'
 
 // stateless functional component
 const ProductsList = ({products}) =>
     <div>
         {products.map(prod => {
-            return (<h2 key={prod.id}>{prod.name}</h2>)
+            // return (<h2 key={prod.id}>{prod.name}</h2>)
+            return(<Product key={prod.id} product={prod} />)
         })}
     </div>
 
