519b877e75fa1fbeaf54bd36e6717b2173f73ca9
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-01-04 07:51:47 -0600

    display price from stripe

--- app/javascript/packs/Store/Product.js
+++ app/javascript/packs/Store/Product.js
@@ -1,19 +1,7 @@
 import React from 'react'
 import Sticker from './Sticker'
 
-// const Product = ({product}) =>
-//     <div className="product">
-//
-//         <Sticker product={product} />
-//
-//         <div className="cart-controls">
-//             <button>+ Add to cart</button>
-//             <button>- Remove from cart</button>
-//         </div>
-//
-//     </div>
-
-// convert to class to allow for handling button click logic
+// converted to class to allow for handling button click logic
 class Product extends React.Component {
     render() {
         return (
@@ -22,6 +10,12 @@ class Product extends React.Component {
                 <Sticker product={this.props.product} />
 
                 <div className="cart-controls">
+
+                    <p className="product-price">
+                        ${(this.props.product.skus.data[0].price/100).toFixed(2)}
+                        {/* for now, we're assuming each product has 1 SKU */}
+                    </p>
+
                     <button>+ Add to cart</button>
                     <button>- Remove from cart</button>
                 </div>
