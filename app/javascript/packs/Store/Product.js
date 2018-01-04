0f92c536174d24a7d57035c4eddef05dd5e8fe57
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-01-03 19:46:38 -0600

    images display, but with class/className warning

--- app/javascript/packs/Store/Product.js
+++ app/javascript/packs/Store/Product.js
@@ -5,7 +5,9 @@ const Product = ({product}) =>
         <h2>{product.name}</h2>
         <p>5-Pack for $5</p>
         <h3>{product.caption}</h3>
-
+        <div class="sticker">
+            <img src={product.images[0]} alt={product.name} />
+        </div>
         <h3>Free shipping!</h3>
     </div>
 
