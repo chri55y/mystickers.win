1c41edcb89dead970277668634b764fa2ccf8da3
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-01-03 19:50:17 -0600

    change class to className ; add/uncomment product style class, but need to modify style because pictures are too big and overlap text

--- app/javascript/packs/Store/Product.js
+++ app/javascript/packs/Store/Product.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Product = ({product}) =>
-    <div>
+    <div className="product">
         <h2>{product.name}</h2>
         <p>5-Pack for $5</p>
         <h3>{product.caption}</h3>
-        <div class="sticker">
+        <div className="sticker">
             <img src={product.images[0]} alt={product.name} />
         </div>
         <h3>Free shipping!</h3>
