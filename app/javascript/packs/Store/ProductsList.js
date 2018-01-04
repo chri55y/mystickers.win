e45335f71abaadc5916f959f070b42320174182b
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-01-03 20:47:06 -0600

    looking for product price, but that is kept at SKU level, so will not include for now.

--- app/javascript/packs/Store/ProductsList.js
+++ app/javascript/packs/Store/ProductsList.js
@@ -5,6 +5,7 @@ import Product from './Product'
 const ProductsList = ({products}) =>
     <div>
         {products.map(prod => {
+            // console.log(prod)
             // return (<h2 key={prod.id}>{prod.name}</h2>)
             return(<Product key={prod.id} product={prod} />)
         })}
