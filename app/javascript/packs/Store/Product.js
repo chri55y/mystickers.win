4efca6a6119c8a48a19b929807a2e75f78cfcbf0
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-01-03 19:44:35 -0600

    display caption, but not image yet

--- app/javascript/packs/Store/Product.js
+++ app/javascript/packs/Store/Product.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Product = ({product}) =>
     <div>
         <h2>{product.name}</h2>
-        {/*<h3>please</h3>*/}
+        <p>5-Pack for $5</p>
+        <h3>{product.caption}</h3>
+
+        <h3>Free shipping!</h3>
     </div>
 
 export default Product
