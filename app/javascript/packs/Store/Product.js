e69a64fa7652f2a7dad2397e42451b9c5c20d59d
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-01-04 08:12:41 -0600

    add logic to pass functions for adding/removing products from cart ; addToCart function working (adds sku's to Store's this.state.cart.items object)

--- app/javascript/packs/Store/Product.js
+++ app/javascript/packs/Store/Product.js
@@ -3,6 +3,15 @@ import Sticker from './Sticker'
 
 // converted to class to allow for handling button click logic
 class Product extends React.Component {
+
+    handleCartAdd = () => {
+        this.props.handleAdd(this.props.product.skus.data[0].id)
+    }
+
+    handleCartRemove = () => {
+        this.props.handleRemove(this.props.product.skus.data[0].id)
+    }
+
     render() {
         return (
             <div className="product">
@@ -10,14 +19,12 @@ class Product extends React.Component {
                 <Sticker product={this.props.product} />
 
                 <div className="cart-controls">
-
                     <p className="product-price">
                         ${(this.props.product.skus.data[0].price/100).toFixed(2)}
                         {/* for now, we're assuming each product has 1 SKU */}
                     </p>
-
-                    <button>+ Add to cart</button>
-                    <button>- Remove from cart</button>
+                    <button onClick={this.handleCartAdd}>+ Add to cart</button>
+                    <button onClick={this.handleCartRemove}>- Remove from cart</button>
                 </div>
 
             </div>
--- app/javascript/packs/Store/ProductsList.js
+++ app/javascript/packs/Store/ProductsList.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Product from './Product'
 
 
-const ProductsList = ({products}) =>
+const ProductsList = ({products, handleAdd, handleRemove}) =>
     <div>
         {products.map(prod => {
             // console.log(prod)
             // return (<h2 key={prod.id}>{prod.name}</h2>)
-            return(<Product key={prod.id} product={prod} />)
+            return(<Product key={prod.id} product={prod}
+                            handleAdd={handleAdd} handleRemove={handleRemove}
+                    />)
         })}
     </div>
 
