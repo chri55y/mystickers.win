8871e497f6d53ee938c556f38d5b8ef966cb2b0c
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-01-03 21:28:01 -0600

    refactor Product by separating out sticker display logic into new Sticker component

--- app/javascript/packs/Store/Product.js
+++ app/javascript/packs/Store/Product.js
@@ -1,14 +1,10 @@
 import React from 'react'
+import Sticker from './Sticker'
 
 const Product = ({product}) =>
     <div className="product">
-        <h2>{product.name}</h2>
-        <p>5-Pack for $5</p>
-        <h3>{product.caption}</h3>
-        <div className="sticker">
-            <img src={product.images[0]} alt={product.name} />
-        </div>
-        <h3>Free shipping!</h3>
+
+        <Sticker product={product} />
 
         <div className="cart-controls">
             <button>+ Add to cart</button>
---
+++ app/javascript/packs/Store/Sticker.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Sticker = ({product}) =>
+    <div className="product">
+        <h2>{product.name}</h2>
+        <p>5-Pack for $5</p>
+        <h3>{product.caption}</h3>
+        <div className="sticker">
+            <img src={product.images[0]} alt={product.name} />
+        </div>
+        <h3>Free shipping!</h3>
+    </div>
+
+export default Sticker
