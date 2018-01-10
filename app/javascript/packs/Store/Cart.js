49c4a8a8e03781fbe243c3f1fed321daa0b47485
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-01-10 14:12:21 -0600

    install and import library 'react-stripe-checkout'

--- app/javascript/packs/Store/Cart.js
+++ app/javascript/packs/Store/Cart.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+    // using this React Component library for our checkout button, rather than using
+    // stripe checkout directly, and having to ensure ourselves that the checkout javascript
+    // loads before rendering button. (which this component takes care of for us)
+    //
+    // ran "yarn add react-stripe-checkout" to install
+
 
 const Cart = ({cart}) =>
     <div className="cart">
